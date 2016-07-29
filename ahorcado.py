6b292d42c6ae4a5000074555beace2fe50c50967
KarenCaballeros <anakarencaballeros@gmail.com>
2016-07-28 20:39:39 -0600

    termine ahorcado.

--- ahorcado.py
+++ ahorcado.py
@@ -1,14 +1,34 @@
 import funciones
 import random
-print("Bienvenido")
+print("Bienvenido a Ahorcado.")
 num = random.randint(0, 99)
-palabra= funciones.palabra(num)
-espacios= (len(palabra) * "_ ")
-print(espacios)
-
-letra= input("Ingrese una letra: ")	
+palabra_elegida= funciones.palabra(num)
+palabra= list(palabra_elegida)
+guiones= []
+intento = 0
 
 for i in palabra:
-	if i == letra:
-		espacios=  
+	guiones.append("_ ")
 
+while intento < 7:
+	print("")
+	print(" ".join(guiones))
+	print("")
+	letra= input("Ingrese una letra: ")	
+
+	if letra in palabra:
+		for i in range(len(palabra)):
+			if palabra[i] == letra: 
+				guiones[i] = letra
+				if palabra == guiones:
+					print(guiones)
+					print("")
+					print("Felicidades, Ganaste.")
+					intento = 7
+
+	else: 
+		print("")
+		print(funciones.muneco(intento))
+		intento= intento + 1
+		if intento == 7:
+			print("Perdiste, la palabra era: " , palabra_elegida)
