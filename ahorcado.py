f3679190cea8b5dd4f75f0679ee8e48f9d0c8d00
KarenCaballeros <anakarencaballeros@gmail.com>
2016-08-04 20:48:22 -0600

    agrege ultimos detalles

--- ahorcado.py
+++ ahorcado.py
@@ -6,31 +6,45 @@ palabra_elegida= funciones.palabra(aleatorio)
 palabra= list(palabra_elegida)
 guiones= []
 intento = 0
+letras_usadas= []
 
 for i in palabra:
 	guiones.append("_ ")
 
-while intento < 7:
-	print("")
-	print(" ".join(guiones))
-	print("")
-	letra= input("Ingrese una letra: ")	
+repetir_jugada = 1	
 
-	if letra in palabra:
-		for i in range(len(palabra)):
-			if palabra[i] == letra: 
-				guiones[i] = letra
-				if palabra == guiones:
-					print("")
-					print(" ".join(guiones))
-					print("")
-					print("Felicidades, Ganaste.")
-					intento = 7
-
-	else: 
+while repetir_jugada == 1:
+	while intento < 7:
 		print("")
-		print("Fallaste.")
-		print(funciones.muneco(intento))
-		intento= intento + 1
-		if intento == 7:
-			print("Perdiste, la palabra era: " , palabra_elegida)
+		print(" ".join(guiones))
+		print("")
+		letra= input("Ingrese una letra: ")
+		if letra in letras_usadas:
+			print("Ya has usado esta letra, ingresa otra!")
+		else:		
+			if letra in palabra:
+				for i in range(len(palabra)):
+					if palabra[i] == letra: 
+						guiones[i] = letra
+						letras_usadas.append(letra)
+						if palabra == guiones:
+							print("")
+							print(" ".join(guiones))
+							print("")
+							print("Felicidades, Ganaste.")
+							intento = 7
+
+			else: 
+				print("")
+				print("Fallaste.")
+				print(funciones.muneco(intento))
+				letras_usadas.append(letra)
+				intento= intento + 1
+				if intento == 7:
+					print("Perdiste, la palabra era: " , palabra_elegida)	
+
+	print("")			
+	print("Desea...")
+	print("1. Jugar de nuevo.")
+	print("2. Salir.")			
+	repetir_jugada= int(input("Ingrese opcion elegida: " ))					
